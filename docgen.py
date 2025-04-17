29364ba8f47ae89bdf2daf16c1e21c2eb6b2a739
carterpiepenburg1 <piepen22@uwm.edu>
2025-04-16 21:30:49 -0500

    Updated formatting

--- docgen.py
+++ docgen.py
@@ -5,15 +5,11 @@ Group 2 - Carter Piepenburg, Austin VanDenPlas
 """
 
 import requests
-import json
 import pathlib
-import re
 import random
-import math
 
 OLLAMA_API = "http://localhost:11434/api/generate"
 MODEL_NAME = "qwen2.5-coder:1.5b"
-#MODEL_NAME = "deepseek-r1:1.5b"
 
 def generate(prompt, model=MODEL_NAME):
     data = {
@@ -21,7 +17,6 @@ def generate(prompt, model=MODEL_NAME):
         "prompt": prompt,
         "stream": False
     }
-
     response = requests.post(OLLAMA_API, json=data)
     if response.status_code == 200:
         return response.json()["response"]
@@ -40,33 +35,37 @@ if __name__ == "__main__":
 
     context = filePath.read_text()
     file = open(filePath)
-    output = open(filePath.name.removesuffix(".java") + "-documentation.md", "w")
+    output = open("outputs/" + filePath.name.removesuffix(".java") + "-documentation.md", "w")
 
     output.write("# " + filePath.name.removesuffix(".java") + "\n")
     lines = file.readlines()
     index = 0
     numFunctions = 0
+    print("Scanning Java file...\n")
     while index < len(lines):
         line = lines[index].strip()
         if line: #Not a blank line
-            if "{" in line and ("public" in line or "private" in line) and "class" not in line:
+            if "{" in line and ("public" in line or "private" in line) and "class" not in line and "//" not in line and ")" in line:
 
                 #Function header
                 output.write("___\n")
-                output.write("## " + line.removesuffix("{") + "\n")
+                output.write("### " + line[:line.rfind(')') + 1] + "\n")
 
                 #Extracting function string
                 openPara = 1
                 functionString = lines[index]
                 while openPara > 0:
                     index += 1
-                    line = lines[index].strip()
-                    if line:  # Not a blank line
-                        functionString = functionString + lines[index]
-                        if "{" in line:
-                            openPara += 1
-                        if "}" in line:
-                            openPara -= 1
+                    if index < len(lines):
+                        line = lines[index].strip()
+                        if line:  # Not a blank line
+                            functionString = functionString + lines[index]
+                            if "{" in line:
+                                openPara += 1
+                            if "}" in line:
+                                openPara -= 1
+                    else:
+                        break;
 
                 #Function generated description
                 output.write("#### Description:\n")
@@ -76,7 +75,7 @@ if __name__ == "__main__":
                 output.write("#### Code:\n")
                 output.write("```\n" + functionString + "```\n")
 
-                numFunctions += 1
+                numFunctions+=1
 
         index += 1
 
